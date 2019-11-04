5313aa1b563a690f55c440c6e4fab4fbf1c7e600
Wojciech Sielski <wsielski@siela.org>
2019-11-04 20:18:39 +0100

    update

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -8,7 +8,6 @@ alias glg="git log --graph --decorate --oneline --abbrev-commit"
 alias glga="glg --all"
 alias gp='git push origin HEAD'
 alias gpa='git push origin --all'
-alias gpo="git push --set-upstream origin $(git branch | awk '/^\* / { print $2 }')"
 alias gd='git diff'
 alias gc='git commit -s'
 alias gca='git commit -s -a'
@@ -29,4 +28,8 @@ fi
 
 gi() {
 	curl -s "https://www.gitignore.io/api/$*"
+}
+
+gpo() {
+	git push --set-upstream origin $(git branch | awk '/^\* / { print $2 }')
 }
--- terraform/aliases.zsh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-alias tf='terraform'
-alias tfa='terraform apply'
-alias tfp='terraform plan'
